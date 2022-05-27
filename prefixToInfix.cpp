4caa70e33bd2224da47f1d5c0f7479395274008c
Semu112 <seamuspitcher@gmail.com>
2022-05-27 15:27:10 +0930

    #1

--- prefixToInfix.cpp
+++ prefixToInfix.cpp
@@ -45,123 +45,86 @@ bool isNumber(std::string string){
 
 }
 
-bool ERRORDESCRIPTIONS = false;
+#define ERRORDESCRIPTIONS true
 
-std::string convertAndComputeHelper(std::queue<std::string>* queue){
+std::string convertAndComputeHelper(std::queue<std::string>* queue, char lastOperator){
 
-    std::string operatoR = queue->front();
-
-    //First element in a new call to convertAndComputeHelper should always be an operator
-    if(isNumber(operatoR)){
+    if(queue->empty()){
         if(ERRORDESCRIPTIONS){
-            std::cout << "operator is not number" << std::endl;
+            std::cout << "not enough numbers for operands" << std::endl;
         }
         return "Error";
     }
 
+    std::string firstElement = queue->front();
     queue->pop();
 
-    //If we get to the second element and the queue is empty, return error
-    if(queue->empty()){
-        if(ERRORDESCRIPTIONS){
-            std::cout << "queue is empty" << std::endl;
-        }
-        return "Error";
-    }
+    std::string LHS;
 
-    std::string secondElement = "";
-    secondElement =  queue->front();
-    std::string secondElementResult = secondElement;
+    //First element in a new call to convertAndComputeHelper should always be an operator
+    if(isNumber(firstElement)){
 
-    // bool needsBrackets;
-    // //Get computed eqn by finding everything before the space before the equals sign
-    // if(operatoR == "+" || operatoR == "-"){
-    //     needsBrackets = true;;
-    // }
-    // else{
-    //     needsBrackets = false;;
-    // }
-
-
-    if(!(isNumber(secondElement))){
-
-        secondElement = convertAndComputeHelper(queue);
-
-        //If returned error
-        if(secondElement == "Error"){
+        if(firstElement.size() > 2 || stoi(firstElement) < 0){
+            if(ERRORDESCRIPTIONS){
+                std::cout << "element out of bounds" << std::endl;
+            }
             return "Error";
         }
 
-        secondElementResult = secondElement.substr(secondElement.find(" =") + 2); //stores result in variable to be used in stoi funciton later
+        return firstElement + " = " + firstElement;
 
-        //Get computed eqn by finding everything before the space before the equals sign
-        // if(needsBrackets){
-            secondElement = "(" + secondElement.substr(0, secondElement.find(" =")) + ")";
-        // }
-        // else{
-        //     secondElement = secondElement.substr(0, secondElement.find(" ="));
-        // }
-
-    }
-    // //If element in queue is not between 0 and 99 inclusive
-    else if(secondElement.size() > 2 || stoi(secondElement) < 0){
-        if(ERRORDESCRIPTIONS){
-            std::cout << "second element out of range" << std::endl;
-        }
-        return "Error";
     }
     else{
-        queue->pop();
-    }
 
-    //If we get to the third element and the queue is empty, return error
-    if(queue->empty()){
-        if(ERRORDESCRIPTIONS){
-            std::cout << "queue is empty" << std::endl;
-        }
-        return "Error";
-    }
+        std::string strExpOne = convertAndComputeHelper(queue, firstElement[0]);
+        std::string strExpTwo = convertAndComputeHelper(queue, firstElement[0]);
 
-    std::string thirdElement = queue->front();
-    std::string thirdElementResult = thirdElement;
-
-    if(!isNumber(thirdElement)){
-
-        thirdElement = convertAndComputeHelper(queue);
-
-        //If returned error
-        if(thirdElement == "Error"){
+        if(strExpOne == "Error" || strExpTwo == "Error"){
             return "Error";
         }
 
-        thirdElementResult = thirdElement.substr(thirdElement.find(" =") + 2);
+        //Finds last number in returned expressions
+        int itrOne = strExpOne.size();
 
-        //Get computed eqn by finding everything before the space before the equals sign
-        // if(needsBrackets){
-        thirdElement = "(" + thirdElement.substr(0, thirdElement.find(" =")) + ")";
-        // }
-        // else{
-        //     thirdElement = thirdElement.substr(0, thirdElement.find(" ="));
-        // }
+        while(strExpOne[itrOne] != ' '){
+            if(itrOne == 0){
+                break;
+            }
 
-    }
-    //If element in queue is not between 0 and 99 inclusive
-    else if(thirdElement.size() > 2 || stoi(thirdElement) < 0){
-        if(ERRORDESCRIPTIONS){
-            std::cout << "third element out of range" << std::endl;
+            itrOne--;
         }
-        return "Error";
-    }
-    else{
-        queue->pop();
-    }
 
-    int intNumberOne = std::stoi(secondElementResult); //mySubstrFunction returns result without ")"
-    int intNumberTwo = std::stoi(thirdElementResult);
+        int itrTwo = strExpTwo.size();
 
-    int result;
+        while(strExpTwo[itrTwo] != ' '){
+            if(itrTwo == 0){
+                break;
+            }
 
-    switch (operatoR[0]){
+            itrTwo--;
+        }
+
+        int intNumberOne = stoi(strExpOne.substr(itrOne));
+        int intNumberTwo = stoi(strExpTwo.substr(itrTwo));
+
+        //brackets not applied if * or / is immediately after + or - in prefix or if there are two of the same operators in a row
+        //brackets applied if * / operators follow each other
+        //brackets not applied unless + or - is immediately after * or /
+
+        //brackets only applied if + - is immediately after * / in prefix
+        //--- * + 5 6 7 will have brackets
+        //--- * / 5 6 7 will not have brackets
+        //--- / * 5 6 7 will not have brackets
+        //--- * + - 5 6 7 8 will have brackets, (5-6+7)*8
+        //--- * 5 + 6 7 will have brackets, 5*(6+7)
+
+        bool brackets = false;
+        if((lastOperator == '*' || lastOperator == '/') && (firstElement[0] == '+' || firstElement[0] == '-')){
+            brackets = true;
+        }
+
+        int result;
+        switch (firstElement[0]){
         case '+':
             result = intNumberOne + intNumberTwo;
             break;
@@ -179,9 +142,144 @@ std::string convertAndComputeHelper(std::queue<std::string>* queue){
                 std::cout << "operatoR not + - / or *" << std::endl;
             }
             return "Error";
+        }
+
+        strExpOne = strExpOne.substr(0, strExpOne.find(" ="));
+        strExpTwo = strExpTwo.substr(0, strExpTwo.find(" ="));
+
+        std::string LHS = strExpOne + " " + firstElement + " " + strExpTwo;
+
+        if(brackets){
+            LHS = "(" + LHS + ")";
+        }
+
+        return LHS + " = " + std::to_string(result);
+
     }
 
-    return secondElement + " " + operatoR + " " + thirdElement + " = " + std::to_string(result);
+    // queue->pop();
+
+    // //If we get to the second element and the queue is empty, return error
+    // if(queue->empty()){
+    //     if(ERRORDESCRIPTIONS){
+    //         std::cout << "queue is empty" << std::endl;
+    //     }
+    //     return "Error";
+    // }
+
+    // std::string secondElement = "";
+    // secondElement =  queue->front();
+    // std::string secondElementResult = secondElement;
+
+    // //Need brackets is not as simple as having them only on + and minus. 5/6*7 can be interpreted as (5/6)*7 or 5/(6*7)
+    // //Get computed eqn by finding everything before the space before the equals sign
+    // // bool needsBrackets;
+    // // if(operatoR == "+" || operatoR == "-"){
+    // //     needsBrackets = true;;
+    // // }
+    // // else{
+    // //     needsBrackets = false;;
+    // // }
+
+    // if(!(isNumber(secondElement))){
+
+    //     secondElement = convertAndComputeHelper(queue);
+
+    //     //If returned error
+    //     if(secondElement == "Error"){
+    //         return "Error";
+    //     }
+
+    //     secondElementResult = secondElement.substr(secondElement.find(" =") + 2); //stores result in variable to be used in stoi funciton later
+
+    //     //Get computed eqn by finding everything before the space before the equals sign
+    //     // if(needsBrackets){
+    //         secondElement = "(" + secondElement.substr(0, secondElement.find(" =")) + ")";
+    //     // }
+    //     // else{
+    //     //     secondElement = secondElement.substr(0, secondElement.find(" ="));
+    //     // }
+
+    // }
+    // //If element in queue is not between 0 and 99 inclusive
+    // else if(secondElement.size() > 2 || stoi(secondElement) < 0){
+    //     if(ERRORDESCRIPTIONS){
+    //         std::cout << "second element out of range" << std::endl;
+    //     }
+    //     return "Error";
+    // }
+    // else{
+    //     queue->pop();
+    // }
+
+    // //If we get to the third element and the queue is empty, return error
+    // if(queue->empty()){
+    //     if(ERRORDESCRIPTIONS){
+    //         std::cout << "queue is empty" << std::endl;
+    //     }
+    //     return "Error";
+    // }
+
+    // std::string thirdElement = queue->front();
+    // std::string thirdElementResult = thirdElement;
+
+    // if(!isNumber(thirdElement)){
+
+    //     thirdElement = convertAndComputeHelper(queue);
+
+    //     //If returned error
+    //     if(thirdElement == "Error"){
+    //         return "Error";
+    //     }
+
+    //     thirdElementResult = thirdElement.substr(thirdElement.find(" =") + 2);
+
+    //     //Get computed eqn by finding everything before the space before the equals sign
+    //     // if(needsBrackets){
+    //         thirdElement = "(" + thirdElement.substr(0, thirdElement.find(" =")) + ")";
+    //     // }
+    //     // else{
+    //     //     thirdElement = thirdElement.substr(0, thirdElement.find(" ="));
+    //     // }
+
+    // }
+    // //If element in queue is not between 0 and 99 inclusive
+    // else if(thirdElement.size() > 2 || stoi(thirdElement) < 0){
+    //     if(ERRORDESCRIPTIONS){
+    //         std::cout << "third element out of range" << std::endl;
+    //     }
+    //     return "Error";
+    // }
+    // else{
+    //     queue->pop();
+    // }
+
+    // int intNumberOne = std::stoi(secondElementResult); //mySubstrFunction returns result without ")"
+    // int intNumberTwo = std::stoi(thirdElementResult);
+
+    // int result;
+
+    // switch (firstElement[0]){
+    //     case '+':
+    //         result = intNumberOne + intNumberTwo;
+    //         break;
+    //     case '-':
+    //         result = intNumberOne - intNumberTwo;
+    //         break;
+    //     case '*':
+    //         result = intNumberOne * intNumberTwo;
+    //         break;
+    //     case '/':
+    //         result = intNumberOne / intNumberTwo;
+    //         break;
+    //     default: //Default case checks if operator is not + - * or /
+    //         if(ERRORDESCRIPTIONS){
+    //             std::cout << "operatoR not + - / or *" << std::endl;
+    //         }
+    //         return "Error";
+    // }
+
+    // return secondElement + " " + firstElement + " " + thirdElement + " = " + std::to_string(result);
 
 }
 
@@ -191,7 +289,7 @@ std::string PrefixToInfix::convertAndCompute(std::queue<std::string> queue){
 
     *newQueue = queue;
 
-    std::string output = convertAndComputeHelper(newQueue);
+    std::string output = convertAndComputeHelper(newQueue, '?');
 
     if(newQueue->size() >= 1){
         if(ERRORDESCRIPTIONS){
